9ca4bc74ae7e7cdbd77cb958ba8e6ea9e6ac115e
Tina Le <justinale725@gmail.com>
2020-09-26 15:16:04 -0400

    tina's first commit

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;   //control speed of mouse default to 100
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hide and lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Time.deltaTime = amount of time that has passed since the last update function was called
+        //to rotate independent of framerate
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; 
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY; //increase flips rotation
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //so that we cannot overrotate
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;//control speed of movement
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; //radius of sphere we will be using to check
+    public LayerMask groundMask; //control what objects this sphere should check for
+
+    Vector3 velocity;
+    bool isGrounded; //grounded or not
+
+    // Update is called once per frame
+    void Update()
+    {
+        //creates a tiny invisible sphere at this point with the radius specified and if it collides with anything in our ground mask,
+        //then isGrounded is true
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //check 
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
+        //getting input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //consider as an arrow that points in the direction we want to move
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //function on character controller
+        //time.deltaTime makes it framerate independent
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime); //physics of freefall
+
+    }
+}
